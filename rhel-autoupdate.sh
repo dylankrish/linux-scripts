db514f9eefa3e9ad03abe6f19d97338ebd290f81
Dylan Krishnan <91637207+dylankrish@users.noreply.github.com>
2024-07-28 15:59:49 -0400

    define config file

--- rhel-autoupdate.sh
+++ rhel-autoupdate.sh
@@ -11,9 +11,9 @@ dnf install htop vim -y
 dnf install dnf-automatic kpatch kpatch-dnf -y
 dnf kpatch auto
 # change upgrade type to security, apply_updates to yes, reboot to when-needed
-sed -i 's/^upgrade_type = default/upgrade_type = security/' "$config_file"
-sed -i 's/^apply_updates = no/apply_updates = yes/' "$config_file"
-sed -i 's/^reboot = never/reboot = when-needed/' "$config_file"
+sed -i 's/^upgrade_type = default/upgrade_type = security/' "/etc/dnf/automatic.conf"
+sed -i 's/^apply_updates = no/apply_updates = yes/' "/etc/dnf/automatic.conf"
+sed -i 's/^reboot = never/reboot = when-needed/' "/etc/dnf/automatic.conf"
 systemctl enable --now dnf-automatic.timer
 
 dnf update -y
