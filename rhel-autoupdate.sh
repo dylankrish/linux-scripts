fff203d2c294810a51c19519acd253b10d0bd9d5
Dylan Krishnan <91637207+dylankrish@users.noreply.github.com>
2024-07-28 15:53:22 -0400

    add epel and kpatch

--- rhel-autoupdate.sh
+++ rhel-autoupdate.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/sudo /bin/bash
 
+# install epel
+dnf config-manager --set-enabled crb
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
+
 # add packages
 dnf install htop vim -y
 
 # enable auto update
-dnf install dnf-automatic kpatch-dnf -y
+dnf install dnf-automatic kpatch kpatch-dnf -y
+dnf kpatch auto
 vim /etc/dnf/automatic.conf
 
 # enable package auto update timer
 systemctl enable --now dnf-automatic.timer
-
-# install epel
-dnf config-manager --set-enabled crb
-dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 
 dnf update
 dnf -y install podman podman-docker podman-tui
