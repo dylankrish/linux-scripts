1eff3ae97e259e81a144d47e25e8bee06fc73985
dylankrish <91637207+dylankrish@users.noreply.github.com>
2024-10-31 15:45:57 -0400

    rename to el-install

---
+++ el-install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sudo /bin/bash
+
+# install epel
+dnf config-manager --set-enabled crb
+dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm -y
+
+# add packages
+dnf install htop vim -y
+
+dnf update -y
+dnf -y install podman podman-docker podman-tui
+
+# enable podman services
+systemctl enable --now podman.socket
+systemctl enable --now podman-auto-update.timer
+
+echo "Done"
