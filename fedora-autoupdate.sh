1c5d2fec0aed9b5470d4f64e6c18dd5d43cb2eb8
dylankrish <91637207+dylankrish@users.noreply.github.com>
2024-10-31 15:44:35 -0400

    add comments

--- fedora-autoupdate.sh
+++ fedora-autoupdate.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/sudo /bin/bash
 
+# enable auto update
 dnf update -y
 dnf install dnf-automatic
 
+# change upgrade type to security, apply_updates to yes, reboot to when-needed
 sed -i 's/^upgrade_type = default/upgrade_type = security/' "/etc/dnf/automatic.conf"
 sed -i 's/^apply_updates = no/apply_updates = yes/' "/etc/dnf/automatic.conf"
 sed -i 's/^reboot = never/reboot = when-needed/' "/etc/dnf/automatic.conf"
