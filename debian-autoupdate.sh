1de0402d30645efb8c97b0c04f814c2b4cf327c9
Dylan Krishnan <91637207+dylankrish@users.noreply.github.com>
2025-04-25 02:57:21 -0400

    Update debian-autoupdate.sh

--- debian-autoupdate.sh
+++ debian-autoupdate.sh
@@ -2,6 +2,37 @@
 
 apt-get install unattended-upgrades apt-listchanges -y
 
-# not finished yet
+set -e # exit script if error
+set -x # show executed commands
 
-echo "script not finished"
+sudo apt update
+sudo apt install -y unattended-upgrades apt-listchanges
+
+sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure --priority=low unattended-upgrades
+
+echo "Configure sources"
+
+sudo tee /etc/apt/apt.conf.d/50unattended-upgrades > /dev/null <<EOF
+Unattended-Upgrade::Allowed-Origins {
+    "\${distro_id}:\${distro_codename}-security";
+    // "\${distro_id}:\${distro_codename}-updates";
+    // "\${distro_id}:\${distro_codename}-backports";
+};
+
+Unattended-Upgrade::Remove-Unused-Dependencies "true";
+Unattended-Upgrade::Automatic-Reboot "false";
+// Unattended-Upgrade::Mail "your-email@example.com";
+// Unattended-Upgrade::MailOnlyOnError "true";
+EOF
+
+echo "Set update frequency"
+
+sudo tee /etc/apt/apt.conf.d/20auto-upgrades > /dev/null <<EOF
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Download-Upgradeable-Packages "1";
+APT::Periodic::AutocleanInterval "7";
+APT::Periodic::Unattended-Upgrade "1";
+EOF
+
+
+echo "Complete! Test with sudo unattended-upgrade --dry-run --debug"
