a53c3630777e9fb59affb47900c9bb64137122dc
Dylan Krishnan <91637207+dylankrish@users.noreply.github.com>
2026-08-22 22:06:06 +0530

    copy automatic.conf

--- fedora-autoupdate.sh
+++ fedora-autoupdate.sh
@@ -13,6 +13,9 @@ set -x # show executed commands
 dnf update -y
 dnf install dnf-automatic -y
 
+# copy dnf automatic config to editable directory
+cp /usr/share/dnf5/dnf5-plugins/automatic.conf /etc/dnf/automatic.conf
+
 # change upgrade type to security, apply_updates to yes, reboot to when-needed
 sed -i 's/^upgrade_type = default/upgrade_type = security/' "/etc/dnf/automatic.conf"
 sed -i 's/^apply_updates = no/apply_updates = yes/' "/etc/dnf/automatic.conf"
